3e028e3ff94eef675b1606115de228c7375eb423
David Marin <davide162>
2023-11-23 15:19:23 -0500

    Configuración de process pipeline

--- src/app/pages/process-pipeline/process-pipeline.component.ts
+++ src/app/pages/process-pipeline/process-pipeline.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { AlertPersonalService } from 'src/app/services/alert-custome.service';
+import { ParametersService } from 'src/app/services/parameters.service';
 
+declare var $: any;
 @Component({
   selector: 'app-process-pipeline',
   templateUrl: './process-pipeline.component.html',
   styleUrls: ['./process-pipeline.component.css']
 })
 export class ProcessPipelineComponent {
+  public current_task: number = 1;
+  public lstParameters: any = [];
+
+  constructor(
+    private _parameters_service: ParametersService,
+    private _alert: AlertPersonalService
+  ) {
+  
+  }
+
+  ngOnInit(): void {
+    this.getParametersByTask(1);
+  }
+
+  getParametersByTask(page: number): void {
+    $('.preloader').show();
+    this._parameters_service.getListingParameters(page).subscribe((resp) => {
+      this.lstParameters = resp.data;
+      console.log(this.lstParameters);
+      $('.preloader').hide();
+    });
+  }
+
+  generateParametersPanel():void{
+
+  }
+
+  changeTabPanelTask(page: number): void {
+    alert();
+  }
+
+  startProcess(task: number):void{
+
+  }
+
+  restartProcess(task: number):void{
+    
+  }
+
+  nextProcess(c_task: number):void{
+    
+  }
 
 }
