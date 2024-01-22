b2eafc0a3fdbf53ee4e63f1bec43369ae03509d0
David Marin <davide162>
2024-01-21 21:50:24 -0500

    Configuracion de ambiente prd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+export const environment_dev = {
   production: false,
   pagination_size: 10,  
   client_id_google:'521818877411-nqqjaghippoeicsl68udp19eppo1m4na.apps.googleusercontent.com',
@@ -36,11 +36,36 @@ export const environment = {
 
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+export const environment = {
+  production: false,
+  pagination_size: 10,  
+  client_id_google:'521818877411-nqqjaghippoeicsl68udp19eppo1m4na.apps.googleusercontent.com',
+  url_base:"10.0.80.84/login",
+  url_login:"10.0.80.84/login",
+  // Apis propias para hacer el canal de comunicación con back-end.
+  url_base_api:'http://10.0.80.150/docs/index.html',
+  url_auth: 'http://10.0.80.150/docs/index.html',
+  //API permissions
+  url_api_permissions: '10.0.80.150/permissions',
+  //API rols
+  url_api_roles: '10.0.80.150/roles',
+  //API users
+  url_api_users: '10.0.80.150/users',
+  //API process
+  url_api_processes: '10.0.80.150/processes',
+  //API tasks
+  url_api_tasks: '10.0.80.150/tasks',
+  //API sub-tasks
+  url_api_subtasks: '10.0.80.150/subtasks',
+  //API parameters
+  url_api_parameters: '10.0.80.150/params',
+  //API patients
+  url_api_patient: '10.0.80.150/patients',
+  //API data
+  url_api_data: '10.0.80.150/data',
+  //API exam
+  url_api_exam: '10.0.80.150/exams',
+  //API data
+  url_api_data_subtask_exam: '10.0.80.150/data',
+
+};
