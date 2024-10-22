10ee8e3f23243395184d1921eb05bfb995177d45
felipebuitragocarmona <felipebuitragocarmona@gmail.com>
2024-10-22 15:00:35 -0500

    despliegue publica v03

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,38 +2,38 @@ export const environment = {
     production: true, 
     pagination_size: 10,
     client_id_google: '521818877411-nqqjaghippoeicsl68udp19eppo1m4na.apps.googleusercontent.com',
-    url_main_backend: "https://190.7.134.182/api",
-    url_base: "https://190.7.134.182/login",
-    url_login: "https://190.7.134.182/login",
+    url_main_backend: "http://190.7.134.182/api",
+    url_base: "http://190.7.134.182/login",
+    url_login: "http://190.7.134.182/login",
     // Apis propias para hacer el canal de comunicación con back-end.
-    url_base_api: 'https://190.7.134.182/api',
-    url_auth: 'https://190.7.134.182/api/login-fake',
+    url_base_api: 'http://190.7.134.182/api',
+    url_auth: 'http://190.7.134.182/api/login-fake',
     //API permissions
-    url_api_permissions: 'https://190.7.134.182/api/permissions',
+    url_api_permissions: 'http://190.7.134.182/api/permissions',
     //API rols
-    url_api_roles: 'https://190.7.134.182/api/roles',
+    url_api_roles: 'http://190.7.134.182/api/roles',
     //API users
-    url_api_users: 'https://190.7.134.182/api/users',
+    url_api_users: 'http://190.7.134.182/api/users',
     //API process
-    url_api_processes: 'https://190.7.134.182/api/processes',
+    url_api_processes: 'http://190.7.134.182/api/processes',
     //API tasks
-    url_api_tasks: 'https://190.7.134.182/api/tasks',
+    url_api_tasks: 'http://190.7.134.182/api/tasks',
     //API sub-tasks
-    url_api_subtasks: 'https://190.7.134.182/api/subtasks',
+    url_api_subtasks: 'http://190.7.134.182/api/subtasks',
     //API sub-tasks-EXAM
-    url_api_subtask_exam: 'https://190.7.134.182/api/subtasks-exam',
+    url_api_subtask_exam: 'http://190.7.134.182/api/subtasks-exam',
     //API parameters
-    url_api_parameters: 'https://190.7.134.182/api/params',
+    url_api_parameters: 'http://190.7.134.182/api/params',
     //API patients
-    url_api_patient: 'https://190.7.134.182/api/patients',
+    url_api_patient: 'http://190.7.134.182/api/patients',
     //API data
-    url_api_data: 'https://190.7.134.182/api/data',
+    url_api_data: 'http://190.7.134.182/api/data',
     //API exam
-    url_api_exam: 'https://190.7.134.182/api/exams',
+    url_api_exam: 'http://190.7.134.182/api/exams',
     //API data
-    url_api_data_subtask_exam: 'https://190.7.134.182/api/datum_sub_task_exam',
+    url_api_data_subtask_exam: 'http://190.7.134.182/api/datum_sub_task_exam',
     //API process pipeline
-    url_api_process_pipeline: 'https://190.7.134.182/api/xx',
+    url_api_process_pipeline: 'http://190.7.134.182/api/xx',
     //API get exams by id_process
-    url_api_get_exams_by_idprocess: 'https://190.7.134.182/api/xx',
+    url_api_get_exams_by_idprocess: 'http://190.7.134.182/api/xx',
 };
