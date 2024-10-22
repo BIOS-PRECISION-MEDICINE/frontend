4f41ea3598367fe54e083190e68974c0b256f70a
felipebuitragocarmona <felipebuitragocarmona@gmail.com>
2024-10-22 10:14:07 -0500

    despliegue

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,39 @@
+export const environment = {
+    production: true,
+    pagination_size: 10,
+    client_id_google: '521818877411-nqqjaghippoeicsl68udp19eppo1m4na.apps.googleusercontent.com',
+    url_main_backend: "https://172.30.90.25/api",
+    url_base: "https://172.30.90.25/login",
+    url_login: "https://172.30.90.25/login",
+    // Apis propias para hacer el canal de comunicación con back-end.
+    url_base_api: 'https://172.30.90.25/api',
+    url_auth: 'https://172.30.90.25/api/login-fake',
+    //API permissions
+    url_api_permissions: 'https://172.30.90.25/api/permissions',
+    //API rols
+    url_api_roles: 'https://172.30.90.25/api/roles',
+    //API users
+    url_api_users: 'https://172.30.90.25/api/users',
+    //API process
+    url_api_processes: 'https://172.30.90.25/api/processes',
+    //API tasks
+    url_api_tasks: 'https://172.30.90.25/api/tasks',
+    //API sub-tasks
+    url_api_subtasks: 'https://172.30.90.25/api/subtasks',
+    //API sub-tasks-EXAM
+    url_api_subtask_exam: 'https://172.30.90.25/api/subtasks-exam',
+    //API parameters
+    url_api_parameters: 'https://172.30.90.25/api/params',
+    //API patients
+    url_api_patient: 'https://172.30.90.25/api/patients',
+    //API data
+    url_api_data: 'https://172.30.90.25/api/data',
+    //API exam
+    url_api_exam: 'https://172.30.90.25/api/exams',
+    //API data
+    url_api_data_subtask_exam: 'https://172.30.90.25/api/datum_sub_task_exam',
+    //API process pipeline
+    url_api_process_pipeline: 'https://172.30.90.25/api/xx',
+    //API get exams by id_process
+    url_api_get_exams_by_idprocess: 'https://172.30.90.25/api/xx',
+};
