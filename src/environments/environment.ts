6abf52e522da9700c91d4a6db6edee0e01ccaa69
felipebuitragocarmona <felipebuitragocarmona@gmail.com>
2024-10-22 14:53:26 -0500

    actualizacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,53 +1,39 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
+  production: false, 
   pagination_size: 10,
-  client_id_google:'521818877411-nqqjaghippoeicsl68udp19eppo1m4na.apps.googleusercontent.com',
-  url_main_backend:"http://10.0.80.43",
-  url_base:"http://localhost:4200/login",
-  url_login:"http://localhost:4200/login",
+  client_id_google: '521818877411-nqqjaghippoeicsl68udp19eppo1m4na.apps.googleusercontent.com',
+  url_main_backend: "https://190.7.134.182/api",
+  url_base: "https://190.7.134.182/login",
+  url_login: "https://190.7.134.182/login",
   // Apis propias para hacer el canal de comunicación con back-end.
-  url_base_api:'http://10.0.80.43',
-  url_auth: 'http://10.0.80.43/login-fake',
+  url_base_api: 'https://190.7.134.182/api',
+  url_auth: 'https://190.7.134.182/api/login-fake',
   //API permissions
-  url_api_permissions: 'http://10.0.80.43/permissions',
+  url_api_permissions: 'https://190.7.134.182/api/permissions',
   //API rols
-  url_api_roles: 'http://10.0.80.43/roles',
+  url_api_roles: 'https://190.7.134.182/api/roles',
   //API users
-  url_api_users: 'http://10.0.80.43/users',
+  url_api_users: 'https://190.7.134.182/api/users',
   //API process
-  url_api_processes: 'http://10.0.80.43/processes',
+  url_api_processes: 'https://190.7.134.182/api/processes',
   //API tasks
-  url_api_tasks: 'http://10.0.80.43/tasks',
+  url_api_tasks: 'https://190.7.134.182/api/tasks',
   //API sub-tasks
-  url_api_subtasks: 'http://10.0.80.43/subtasks',
+  url_api_subtasks: 'https://190.7.134.182/api/subtasks',
   //API sub-tasks-EXAM
-  url_api_subtask_exam: 'http://10.0.80.43/subtasks-exam',
+  url_api_subtask_exam: 'https://190.7.134.182/api/subtasks-exam',
   //API parameters
-  url_api_parameters: 'http://10.0.80.43/params',
+  url_api_parameters: 'https://190.7.134.182/api/params',
   //API patients
-  url_api_patient: 'http://10.0.80.43/patients',
+  url_api_patient: 'https://190.7.134.182/api/patients',
   //API data
-  url_api_data: 'http://10.0.80.43/data',
+  url_api_data: 'https://190.7.134.182/api/data',
   //API exam
-  url_api_exam: 'http://10.0.80.43/exams',
+  url_api_exam: 'https://190.7.134.182/api/exams',
   //API data
-  url_api_data_subtask_exam: 'http://10.0.80.43/datum_sub_task_exam',
+  url_api_data_subtask_exam: 'https://190.7.134.182/api/datum_sub_task_exam',
   //API process pipeline
-  url_api_process_pipeline: 'http://10.0.80.43/xx',
+  url_api_process_pipeline: 'https://190.7.134.182/api/xx',
   //API get exams by id_process
-  url_api_get_exams_by_idprocess: 'http://10.0.80.43/xx',
-
+  url_api_get_exams_by_idprocess: 'https://190.7.134.182/api/xx',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
