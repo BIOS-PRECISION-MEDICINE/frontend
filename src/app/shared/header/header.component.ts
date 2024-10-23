1100913e79f0accf65b3fae66c997e4e47504e6b
felipebuitragocarmona <felipebuitragocarmona@gmail.com>
2024-10-22 21:41:08 -0500

    Ajustes notificaciones

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,45 +3,57 @@ import { UsuarioService } from '../../services/usuario.service';
 import { Usuario } from '../../models/usuario.model';
 import { Router } from '@angular/router';
 import { googleCredentials } from 'src/app/interfaces/googleCredentials';
+import { WebSocketService } from 'src/app/services/web-socket.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-  ]
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
   public usuario: Usuario;
+  public notifications:any[]=[]
 
-  constructor( private usuarioService: UsuarioService,
-               private router: Router ) {
-    this.usuario={
-      email:"",
-      nombre:"",
+  constructor(private usuarioService: UsuarioService,
+    private router: Router,
+    private webSocketService: WebSocketService
+  ) {
+    this.webSocketService.setNameEvent("event")
+    this.webSocketService.callback.subscribe(res => {
+      console.log("Llegando notificación desde el backend->" + JSON.stringify(res))
+      this.notifications.unshift(res)
+      console.log(JSON.stringify(this.notifications))
+    })
+    this.usuario = {
+      email: "",
+      nombre: "",
 
     }
-    if(localStorage.getItem('variablesDeUsuarioLogadoDTO') === null){
+    if (localStorage.getItem('variablesDeUsuarioLogadoDTO') === null) {
       this.router.navigate(["/login"])
-    }else{
+    } else {
       let usuario: googleCredentials = JSON.parse(localStorage.getItem('variablesDeUsuarioLogadoDTO')!);
-      console.log("Nuevo logueado "+JSON.stringify(usuario))
-      this.usuario = new Usuario(usuario.name,usuario.email,'',usuario.picture);
+      console.log("Nuevo logueado " + JSON.stringify(usuario))
+      this.usuario = new Usuario(usuario.name, usuario.email, '', usuario.picture);
     }
-    
+
+  }
+  redirect(item:any){
+    console.log("redireccionando "+JSON.stringify(item))
   }
 
   logout() {
     this.usuarioService.logout();
   }
 
-  buscar( termino: string ) {
+  buscar(termino: string) {
 
-    if ( termino.length === 0  ) {
+    if (termino.length === 0) {
       return;
     }
 
-    this.router.navigateByUrl(`/dashboard/buscar/${ termino }`);
+    this.router.navigateByUrl(`/dashboard/buscar/${termino}`);
   }
 
 }
